914c347036f0acc1624338ba9da6d487669afbc1
shibilpk <59109749+shibilpk@users.noreply.github.com>
2019-12-28 12:53:47 +0530

    Update notes.py
    aded

--- notes.py
+++ notes.py
@@ -1,2 +1,5 @@
 # To remane project
 python manage.py rename demo new djecommerce
+
+#to dumbdata with django default datas
+python manage.py dumpdata --exclude auth.permission --exclude users.permission --exclude auth.user --exclude auth.group --exclude users.notificationsubject --exclude main.mode --exclude sessions.session --exclude contenttypes.contenttype --indent 2 > data.json
